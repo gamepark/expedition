459edeac02dcab4ebabac639e8a438c3719d9608
Romain Fromi <romain.fromi@gmail.com>
2023-03-15 17:43:00 +0100

    Setup using moves, shuffle the cards once created

--- rules/src/ExpeditionRules.ts
+++ rules/src/ExpeditionRules.ts
@@ -5,9 +5,12 @@ import {places} from './material/Place'
 import {MaterialRules} from '@gamepark/rules-api'
 
 export class ExpeditionRules extends MaterialRules<Color> {
-  setup(): void {
-    this.addItems(MaterialType.Card, places.map((place, i) => ({id: place, location: {type: LocationType.CardsDeck, x: i}})))
-    /*material.shuffle(MaterialType.Place, LocationType.CardsDeck)
+  setup() {
+    return this.movesBuilder()
+      .createItems(MaterialType.Card, places.map((place, i) => ({id: place, location: {type: LocationType.CardsDeck, x: i}})))
+      .shuffle(MaterialType.Card)
+      .moves
+    /*
     const deal = this.game.players.length <= 3 ? 12 : 9
     for (const playerId in this.game.players) {
       for (let i = 0; i < deal; i++) {
