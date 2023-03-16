a518353924df57336c22a06ac3e67740a5f965fe
Romain Fromi <romain.fromi@gmail.com>
2023-03-16 17:27:07 +0100

    Deal cards during setup

--- rules/src/ExpeditionRules.ts
+++ rules/src/ExpeditionRules.ts
@@ -3,20 +3,22 @@ import {MaterialType} from './material/ExpeditionMaterial'
 import {LocationType} from './material/ExpeditionLocations'
 import {places} from './material/Place'
 import {MaterialRules} from '@gamepark/rules-api'
+import {ExpeditionOptions} from './ExpeditionOptions'
 
 export class ExpeditionRules extends MaterialRules<Color> {
-  setup() {
+  setup(options: ExpeditionOptions) {
     const cards = this.material(MaterialType.Card)
     cards
       .create(...places.map((place, i) => ({id: place, location: {type: LocationType.CardsDeck, x: i}})))
       .shuffle()
-    /*
-    const deal = this.game.players.length <= 3 ? 12 : 9
-    for (const playerId in this.game.players) {
+    const deal = options.players.length <= 3 ? 12 : 9
+    for (const player of options.players) {
       for (let i = 0; i < deal; i++) {
-        const card = material.getItem(MaterialType.Place, LocationType.Deck)
-        material.move(card, LocationType.Hand, playerId) // TODO: x maximum
+        const card = cards.items.filter(card => card.location.type === LocationType.CardsDeck)
+          .reduce((topCard, card) => card.location.x! > topCard.location.x! ? card : topCard)
+        card.location = {type: LocationType.Hand, player: player.id, x: i}
       }
-    }*/
+      // TODO: if player does not have at least 4 places 3 nodes away from the start, discard hand under the deck and draw again
+    }
   }
 }
