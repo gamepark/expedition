8ffe73557256a655c8985ee9d6078e32a4e826ef
Romain Fromi <romain.fromi@gmail.com>
2023-03-22 17:43:37 +0100

    First example of legal moves. Helper functions are needed to simplify the syntax.

--- rules/src/rules/SetupKeyPlaces.ts
+++ rules/src/rules/SetupKeyPlaces.ts
@@ -1,9 +1,20 @@
-import {PlayerRulesStep} from '../../../../workshop/packages/rules-api'
 import Color from '../Color'
 import {MaterialType} from '../material/ExpeditionMaterial'
+import {LocationType} from '../material/ExpeditionLocations'
+import {MaterialMove, MaterialMoveType, MoveKind, PlayerRulesStep} from '@gamepark/rules-api'
 
 export class SetupKeyPlaces extends PlayerRulesStep<Color, MaterialType> {
   getPlayerMoves() {
-    return []
+    const tokens = this.game.items[MaterialType.Token]!.filter(token => token.location.type === LocationType.TokenArea && token.location.player)
+    const cards = this.game.items[MaterialType.Card]!.filter(card => card.location.type === LocationType.Hand && card.location.player === this.player
+      && !this.game.items[MaterialType.Token]!.some(token => token.location.type === LocationType.Place && token.location.id === card.id))
+    return tokens.flatMap((_, index) => cards.map<MaterialMove>(card => ({
+        kind: MoveKind.MaterialMove,
+        itemsType: MaterialType.Token,
+        type: MaterialMoveType.Move,
+        item: index,
+        location: {type: LocationType.Place, id: card.id}
+      }))
+    )
   }
 }
