cfc9de42ea2449b475151eada235946235630c84
Patrick Beaumé <p.beaume@gmail.com>
2023-03-24 14:22:07 +0100

    fix: reformat files after changing setting

--- rules/src/ExpeditionRules.ts
+++ rules/src/ExpeditionRules.ts
@@ -1,13 +1,13 @@
-import Color from "./Color";
-import { MaterialType } from "./material/ExpeditionMaterial";
-import { LocationType } from "./material/ExpeditionLocations";
-import { places } from "./material/Place";
-import { MaterialRules } from "@gamepark/rules-api";
-import { ExpeditionOptions } from "./ExpeditionOptions";
-import { arrowColors } from "./material/ArrowColor";
-import { SetupKeyPlaces } from "./rules/SetupKeyPlaces";
-import { PlayerTurn } from "./rules/PlayerTurn";
-import { TicketEffect } from "./rules/TicketEffect";
+import Color from './Color'
+import {MaterialType} from './material/ExpeditionMaterial'
+import {LocationType} from './material/ExpeditionLocations'
+import {places} from './material/Place'
+import {MaterialRules} from '@gamepark/rules-api'
+import {ExpeditionOptions} from './ExpeditionOptions'
+import {arrowColors} from './material/ArrowColor'
+import {SetupKeyPlaces} from './rules/SetupKeyPlaces'
+import {PlayerTurn} from './rules/PlayerTurn'
+import {TicketEffect} from './rules/TicketEffect'
 
 export class ExpeditionRules extends MaterialRules<
   Color,
@@ -15,22 +15,22 @@ export class ExpeditionRules extends MaterialRules<
   LocationType
 > {
   setup(options: ExpeditionOptions) {
-    const cards = this.material(MaterialType.Card);
+    const cards = this.material(MaterialType.Card)
     cards
       .create(
         places.map((place, i) => ({
           id: place,
-          location: { type: LocationType.CardsDeck, x: i },
+          location: {type: LocationType.CardsDeck, x: i}
         }))
       )
-      .shuffle();
+      .shuffle()
 
-    const deal = options.players.length <= 3 ? 12 : 9;
-    const cardDeck = cards.search().location(LocationType.CardsDeck);
+    const deal = options.players.length <= 3 ? 12 : 9
+    const cardDeck = cards.search().location(LocationType.CardsDeck)
     for (const player of options.players) {
       for (let i = 0; i < deal; i++) {
-        const card = cardDeck.maxBy((location) => location.x);
-        card!.location = { type: LocationType.Hand, player: player.id, x: i };
+        const card = cardDeck.maxBy((location) => location.x)
+        card!.location = {type: LocationType.Hand, player: player.id, x: i}
       }
       // TODO: if player does not have at least 4 places 3 nodes away from the start, discard hand under the deck and draw again
     }
@@ -39,34 +39,36 @@ export class ExpeditionRules extends MaterialRules<
       const card = cards
         .search()
         .location(LocationType.CardsDeck)
-        .maxBy((location) => location.x)!;
+        .maxBy((location) => location.x)!
       // TODO: put card at the bottom of the deck & draw another one if it is not 3 nodes away from the start
-      card.location = { type: LocationType.CommonPlacesArea, x: i };
+      card.location = {type: LocationType.CommonPlacesArea, x: i}
     }
 
     for (const player of options.players) {
-      const tokens = this.material(MaterialType.Token);
+      const tokens = this.material(MaterialType.Token)
       tokens.create(4, (x) => ({
         id: player.id,
-        location: { type: LocationType.TokenArea, x, player: player.id },
-      }));
-      const tickets = this.material(MaterialType.Ticket);
+        location: {type: LocationType.TokenArea, x, player: player.id}
+      }))
+      const tickets = this.material(MaterialType.Ticket)
       tickets.create(3, (x) => ({
-        location: { type: LocationType.TicketArea, x, player: player.id },
-      }));
+        location: {type: LocationType.TicketArea, x, player: player.id}
+      }))
     }
 
-    const arrows = this.material(MaterialType.Arrow);
+    const arrows = this.material(MaterialType.Arrow)
     arrows.create(
       arrowColors.map((arrow) => ({
         id: arrow,
         quantity: 45,
-        location: { type: LocationType.ArrowsStock },
+        location: {type: LocationType.ArrowsStock}
       }))
-    );
+    )
 
-    this.start(SetupKeyPlaces, this.game.players[0]);
+    this.start(SetupKeyPlaces, this.game.players[0])
   }
 
-  rulesSteps = [SetupKeyPlaces, PlayerTurn, TicketEffect];
+  getRulesSteps() {
+    return [SetupKeyPlaces, PlayerTurn, TicketEffect]
+  }
 }
