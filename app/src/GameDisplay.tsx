5740cbe4914c65026846d591cbbe15e6b42edfc1
Romain Fromi <romain.fromi@gmail.com>
2023-04-18 16:58:23 +0200

    Add game table bounds & zoom info

--- app/src/GameDisplay.tsx
+++ app/src/GameDisplay.tsx
@@ -3,4 +3,7 @@ import {GameTable} from '@gamepark/react-client'
 import {MaterialsDescription} from './material/MaterialsDescription'
 import {Locators} from './locators/Locators'
 
-export const GameDisplay = () => <GameTable material={MaterialsDescription} locators={Locators} zoomMin={1.4} margin={{top: 7, left: 0, right: 20, bottom: 0}}/>
+export const GameDisplay = () =>
+  <GameTable material={MaterialsDescription} locators={Locators}
+             xMin={-63} xMax={50} yMin={-34} yMax={34}
+             zoomMin={1.4} zoomMax={5} margin={{top: 7, left: 0, right: 20, bottom: 0}}/>
