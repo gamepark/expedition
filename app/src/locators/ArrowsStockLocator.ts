7653e066284149355dfcbb7a5808b5902014d01c
Romain Fromi <romain.fromi@gmail.com>
2023-04-25 18:40:30 +0200

    Display arrows in stock in piles

--- app/src/locators/ArrowsStockLocator.ts
+++ app/src/locators/ArrowsStockLocator.ts
@@ -1,19 +1,29 @@
-import { ItemLocator } from '@gamepark/react-components'
-import { Coordinates, MaterialItem } from '@gamepark/rules-api'
+import {Coordinates, MaterialItem} from '@gamepark/rules-api'
 import Color from '@gamepark/expedition/Color'
-import { MaterialType } from '@gamepark/expedition/material/ExpeditionMaterial'
-import { LocationType } from '@gamepark/expedition/material/ExpeditionLocations'
-import { ArrowColor } from '@gamepark/expedition/material/ArrowColor'
+import {MaterialType} from '@gamepark/expedition/material/ExpeditionMaterial'
+import {LocationType} from '@gamepark/expedition/material/ExpeditionLocations'
+import {ArrowColor} from '@gamepark/expedition/material/ArrowColor'
+import {PileLocator} from '@gamepark/react-components'
 
-export class ArrowsStockLocator extends ItemLocator<Color, MaterialType, LocationType> {
-  getPosition(item: MaterialItem<Color, LocationType, ArrowColor>): Coordinates {
+export class ArrowsStockLocator extends PileLocator<Color, MaterialType, LocationType> {
+  rotate = true
+
+  getCoordinates(item: MaterialItem<Color, LocationType, ArrowColor>): Coordinates {
     switch (item.id!) {
       case ArrowColor.Yellow:
-        return { x: -60, y: -10, z: 0 }
+        return {x: -59, y: -10, z: 0}
       case ArrowColor.Blue:
-        return { x: -60, y: 0, z: 0 }
+        return {x: -59, y: 0, z: 0}
       case ArrowColor.Red:
-        return { x: -60, y: 10, z: 0 }
+        return {x: -59, y: 10, z: 0}
     }
+  }
+
+  getRadius(): number {
+    return 3
+  }
+
+  getPileId(item: MaterialItem<Color, LocationType>): number {
+    return item.id!
   }
 }
