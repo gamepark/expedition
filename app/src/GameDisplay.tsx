7685b12cf8f18a66057023b04981fe33708fb9e6
Romain Fromi <romain.fromi@gmail.com>
2023-05-09 15:48:19 +0200

    Import GameTable from @gamepark/react-game

--- app/src/GameDisplay.tsx
+++ app/src/GameDisplay.tsx
@@ -1,9 +1,9 @@
 /** @jsxImportSource @emotion/react */
-import {GameTable} from '@gamepark/react-client'
-import {MaterialsDescription} from './material/MaterialsDescription'
-import {Locators} from './locators/Locators'
+import { GameTable } from '@gamepark/react-game'
+import { MaterialsDescription } from './material/MaterialsDescription'
+import { Locators } from './locators/Locators'
 
 export const GameDisplay = () =>
   <GameTable material={MaterialsDescription} locators={Locators}
              xMin={-63} xMax={50} yMin={-34} yMax={34}
-             zoomMin={1.4} zoomMax={5} margin={{top: 7, left: 0, right: 20, bottom: 0}}/>
+             zoomMin={1.4} zoomMax={5} margin={{ top: 7, left: 0, right: 20, bottom: 0 }}/>
