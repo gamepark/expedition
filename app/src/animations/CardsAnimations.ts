4113a63105f622e402608e33dc82b55f805e530a
Romain Fromi <romain.fromi@gmail.com>
2023-06-13 16:25:20 +0200

    New api for Animations

--- app/src/animations/CardsAnimations.ts
+++ app/src/animations/CardsAnimations.ts
@@ -1,11 +1,11 @@
 import { ItemAnimationContext, MaterialAnimations } from '@gamepark/react-game'
 import { Animation } from '@gamepark/react-client'
 import { keyframes } from '@emotion/react'
-import { isMoveItem, ItemMove, MaterialItem } from '@gamepark/rules-api'
+import { DisplayedItem, isMoveItem, ItemMove } from '@gamepark/rules-api'
 import { LocationType } from '@gamepark/expedition/material/LocationType'
 
 export class CardsAnimations extends MaterialAnimations {
-  override getKeyframesToDestination(destination: string, item: MaterialItem, animation: Animation<ItemMove>, context: ItemAnimationContext) {
+  override getKeyframesToDestination(destination: string, item: DisplayedItem, animation: Animation<ItemMove>, context: ItemAnimationContext) {
     if (isMoveItem(animation.move) && animation.move.position.location?.type === LocationType.Hand) {
       return keyframes`
         80% {
