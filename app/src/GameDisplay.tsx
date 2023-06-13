ae99ca0e7261b661405158f940d31d6fb6248be8
Romain Fromi <romain.fromi@gmail.com>
2023-06-13 18:47:26 +0200

    Dialog when the end of the game is triggered

--- app/src/GameDisplay.tsx
+++ app/src/GameDisplay.tsx
@@ -1,11 +1,13 @@
 /** @jsxImportSource @emotion/react */
 import { GameTable } from '@gamepark/react-game'
 import { PlayerPanels } from './panels/PlayerPanels'
+import { LastTurnDialog } from './dialogs/LastTurnDialog'
 
 export const GameDisplay = () => {
   return <>
     <GameTable xMin={-63} xMax={50} yMin={-34} yMax={34}
                zoomMin={1.35} zoomMax={5} margin={{ top: 7, left: 0, right: 20, bottom: 0 }}/>
     <PlayerPanels/>
+    <LastTurnDialog/>
   </>
 }
---
+++ app/src/dialogs/LastTurnDialog.tsx
@@ -0,0 +1,47 @@
+/** @jsxImportSource @emotion/react */
+import { RulesDialog, ThemeButton, usePlayerName, useRules } from '@gamepark/react-game'
+import { useEffect, useState } from 'react'
+import { ExpeditionRules } from '@gamepark/expedition/ExpeditionRules'
+import { useTranslation } from 'react-i18next'
+import { getPlayerName } from '@gamepark/expedition/ExpeditionOptions'
+import Color from '@gamepark/expedition/Color'
+import { css } from '@emotion/react'
+
+export const LastTurnDialog = () => {
+  const { t } = useTranslation()
+  const rules = useRules<ExpeditionRules>()
+  console.log(!rules?.isOver())
+  const lastTurn = !rules?.isOver() && rules?.getMemory<{ lastTurn?: boolean }>()?.lastTurn
+  const [explainLastTurn, setExplainLastTurn] = useState(false)
+  const lastPlayer = rules ? rules.game.players[rules.game.players.length - 1] : Color.Red
+  const lastPlayerName = usePlayerName(lastPlayer) || getPlayerName(lastPlayer, t)
+  useEffect(() => {
+      if (lastTurn) setExplainLastTurn(true)
+    }, [lastTurn]
+  )
+  return (
+    <RulesDialog open={explainLastTurn} close={() => setExplainLastTurn(false)}>
+      <div css={rulesCss}>
+        <h2>{t('rules.lastTurn')}</h2>
+        <p>{t('rules.lastTurn.lastPlayer', { player: lastPlayerName })}</p>
+        <p>{t('rules.lastTurn.text')}</p>
+        <ThemeButton onClick={() => setExplainLastTurn(false)}>{t('OK')}</ThemeButton>
+      </div>
+    </RulesDialog>
+  )
+}
+
+const rulesCss = css`
+  max-width: 40em;
+  margin: 1em;
+  font-size: 3em;
+
+  > h2 {
+    margin: 0 1em;
+    text-align: center;
+  }
+
+  > p {
+    white-space: break-spaces;
+  }
+`
