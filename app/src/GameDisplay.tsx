bc05d1951232b02add6302976d96a3100fa0d064
Romain Fromi <romain.fromi@gmail.com>
2023-06-29 13:16:05 +0200

    Improve GameTable position so that it is never under the player panels

--- app/src/GameDisplay.tsx
+++ app/src/GameDisplay.tsx
@@ -5,8 +5,8 @@ import { LastTurnDialog } from './dialogs/LastTurnDialog'
 
 export const GameDisplay = () => {
   return <>
-    <GameTable xMin={-63} xMax={50} yMin={-34} yMax={34}
-               zoomMin={1.35} zoomMax={5} margin={{ top: 7, left: 0, right: 20, bottom: 0 }}/>
+    <GameTable xMin={-63} xMax={45} yMin={-34} yMax={34}
+               zoomMin={1.35} zoomMax={5} margin={{ top: 7, left: 0, right: 30, bottom: 0 }}/>
     <PlayerPanels/>
     <LastTurnDialog/>
   </>
