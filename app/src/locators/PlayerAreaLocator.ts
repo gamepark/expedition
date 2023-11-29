0a2f483f5082709f85304cc8413ceeae36dee6d6
Patrick Beaume <p.beaume@gmail.com>
2023-11-29 13:39:47 +0100

    fix player area rotation

--- app/src/locators/PlayerAreaLocator.ts
+++ app/src/locators/PlayerAreaLocator.ts
@@ -53,7 +53,8 @@ export class PlayerAreaLocator extends LineLocator<Color, MaterialType, Location
     }
   }
 
-  getRotation({ location }: MaterialItem<Color, LocationType>, { type, player }: ItemContext<Color, MaterialType, LocationType>): number {
-    return type === MaterialType.Card && location.player !== player ? -90 : 0
+  getRotations(item: MaterialItem<Color, LocationType>, { type, player }: ItemContext<Color, MaterialType, LocationType>): string[] {
+    const rotateZ = type === MaterialType.Card && item.location.player !== player ? -90 : 0
+    return [`rotateZ(${rotateZ}deg)`]
   }
 }
