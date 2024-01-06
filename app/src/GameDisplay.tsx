e71f306c842c66ea48fe74b01e73fef18eb4d82e
Romain Fromi <romain.fromi@gmail.com>
2024-01-06 11:02:36 +0100

    Upgrade framework and add nav buttons

--- app/src/GameDisplay.tsx
+++ app/src/GameDisplay.tsx
@@ -1,13 +1,31 @@
 /** @jsxImportSource @emotion/react */
-import { GameTable } from '@gamepark/react-game'
-import { PlayerPanels } from './panels/PlayerPanels'
+import { css } from '@emotion/react'
+import { GameTable, GameTableNavigation, usePlayerId } from '@gamepark/react-game'
 import { LastTurnDialog } from './dialogs/LastTurnDialog'
+import { PlayerPanels } from './panels/PlayerPanels'
 
 export const GameDisplay = () => {
+  const playerId = usePlayerId()
   return <>
     <GameTable xMin={-63} xMax={45} yMin={-34} yMax={34}
-               margin={{ top: 7, left: 0, right: 30, bottom: 0 }}/>
+               margin={{ top: 7, left: 0, right: 30, bottom: 0 }}>
+      <GameTableNavigation css={playerId ? navigationCss : spectatorNavCss}/>
+    </GameTable>
     <PlayerPanels/>
     <LastTurnDialog/>
   </>
 }
+
+const navigationCss = css`
+  top: auto;
+  bottom: 1em;
+  flex-direction: column;
+  left: auto;
+  right: 30em;
+`
+
+const spectatorNavCss = css`
+  top: auto;
+  left: 2em;
+  bottom: 2em;
+`
