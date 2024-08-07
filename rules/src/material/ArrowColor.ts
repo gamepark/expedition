c55bee162d861c6b04d4389145370038ebc704fc
Romain Fromi <romain.fromi@gmail.com>
2024-08-07 18:35:27 +0200

    getEnumValues replaces isEnumValue

--- rules/src/material/ArrowColor.ts
+++ rules/src/material/ArrowColor.ts
@@ -1,7 +1,7 @@
-import {isEnumValue} from '@gamepark/rules-api'
+import { getEnumValues } from '@gamepark/rules-api'
 
 export enum ArrowColor {
-    Yellow = 1, Blue, Red
+  Yellow = 1, Blue, Red
 }
 
-export const arrowColors = Object.values(ArrowColor).filter<ArrowColor>(isEnumValue)
+export const arrowColors = getEnumValues(ArrowColor)
