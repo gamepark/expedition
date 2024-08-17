02c7623ee2df5ba481a8d3e09c76df0437d88815
Romain Fromi <romain.fromi@gmail.com>
2024-08-17 09:45:01 +0200

    Some locations are no longer necessary to describe

--- app/src/locators/CardsDeckLocator.ts
+++ app/src/locators/CardsDeckLocator.ts
@@ -2,11 +2,10 @@
 import Color from '@gamepark/expedition/Color'
 import { LocationType } from '@gamepark/expedition/material/LocationType'
 import { MaterialType } from '@gamepark/expedition/material/MaterialType'
-import { DeckLocator } from '@gamepark/react-game'
-import { DeckLocationDescription } from './DeckLocationDescription'
+import { DeckLocator, DropAreaDescription } from '@gamepark/react-game'
+import { placeCardDescription } from '../material/PlaceCardDescription'
 
 export class CardsDeckLocator extends DeckLocator<Color, MaterialType, LocationType> {
-  location = { type: LocationType.Deck }
-  locationDescription = new DeckLocationDescription()
+  locationDescription = new DropAreaDescription(placeCardDescription)
   coordinates = { x: -58, y: -28.5 }
 }
--- app/src/locators/TicketStockLocator.tsx
+++ app/src/locators/TicketStockLocator.tsx
@@ -3,11 +3,9 @@ import Color from '@gamepark/expedition/Color'
 import { LocationType } from '@gamepark/expedition/material/LocationType'
 import { MaterialType } from '@gamepark/expedition/material/MaterialType'
 import { PileLocator } from '@gamepark/react-game'
-import { ticketDescription } from '../material/TicketDescription'
 import { TicketStockDescription } from './TicketStockDescription'
 
 export class TicketStockLocator extends PileLocator<Color, MaterialType, LocationType> {
-  location = ticketDescription.stockLocation
   locationDescription = new TicketStockDescription()
   coordinates = { x: -59, y: 18, z: 0 }
   radius = 3
